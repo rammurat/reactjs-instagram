8b2efc9c7772917bc6eeb75369850fc02d1dfd8d
rammurat <rammurat.rms.sharma0@gmail.com>
2015-12-29 12:10:36 +0530

    Folder name updated

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 var INSTAGRAM = {
-	"appUrl" : "http://localhost:3000/instagram/",
-	"imagePath" : "http://localhost/instagram/public/images/"
+	"appUrl" : "http://localhost:3000/reactjs-instagram/",
+	"imagePath" : "http://localhost/reactjs-instagram/public/images/"
 }
